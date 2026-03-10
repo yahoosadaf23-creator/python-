0362c232fc6b29a2dce3d4a049da6de3dd2c2e1d
sadaf <yahoosadaf23@gmail.com>
2026-03-10 17:35:22 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,131 @@
+
+#print("What's your name?")
+#print("Hello, world!")
+# name =input ("What's your name?")
+#print("hello")
+#print(name) 
+#if you want to use name after hello
+#name=input("What's your name?")
+#print("hello", end="")
+#print(name)
+
+#name=input("What's your name?")
+#print("hello", end ="???")
+#print(name)
+
+# to separate 
+#name= input("what's your name?")
+#print("hello", end="????" , sep="")
+#print(name)
+
+#print('"hello, "friend"')
+#print("hello,\"friend\"")
+
+#name= input("what's your name?")
+#print("hello, {name}")
+
+#print("hello", name )
+
+#print("hello",{name})
+
+
+#name= input("what's your name?")
+#name = name.capitalize()
+#name= name.strip()
+#print("hell0",name )
+#print("hello",name)
+
+#print("hello", {name})  
+
+#name= input("what's your name?")
+#name = name.strip().title()
+#print("hello", name )
+"""
+name = input("what's your name?") . strip().title()
+print(f"hello", name)
+
+
+name = input("what's your name?")
+hello()
+print(name)
+
+#define a function using a def key word 
+
+def hello():
+    print("hello")
+
+name = input("what's your name?")
+hello()
+print(name)
+
+
+def hello(to):
+    print("hello,", to)
+
+name = input("what's your name?")
+hello(name)
+"""
+def main():
+    name = input("what's your name?")
+    hello(name)
+
+
+
+def hello(to="world"):
+    print("hello,", to)
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
